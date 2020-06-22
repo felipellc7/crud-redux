22f8ff01c7d32fbb5a5c22b61903969dff31afd0
Felipe Jose Llerena Catalan <felipe@appmeup.co>
2020-06-22 17:09:36 -0500

    routing done

--- src/App.js
+++ src/App.js
@@ -1,8 +1,22 @@
 import React from 'react';
+import Header from './components/Header'
+import { BrowserRouter as Router, Route, Switch} from 'react-router-dom'
+import Products from './components/Products';
+import NewProduct from './components/NewProduct';
+import EditProduct from './components/EditProduct';
 
 function App() {
   return (
-    <h1>Redux</h1>
+    <Router>
+      <Header />
+      <div className="container mt-5">
+        <Switch>
+          <Route exact path="/" component={Products} />
+          <Route exact path="/products/new" component={NewProduct} />
+          <Route exact path="/products/edit/:id" component={EditProduct} />
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
