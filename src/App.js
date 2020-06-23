63f520cf30340bb04f955d3e11680fc59d575ec8
Felipe Jose Llerena Catalan <felipe@appmeup.co>
2020-06-22 21:50:11 -0500

    set initial redux config

--- src/App.js
+++ src/App.js
@@ -5,17 +5,22 @@ import Products from './components/Products';
 import NewProduct from './components/NewProduct';
 import EditProduct from './components/EditProduct';
 
+import { Provider } from 'react-redux'
+import store from './store'
+
 function App() {
   return (
     <Router>
-      <Header />
-      <div className="container mt-5">
-        <Switch>
-          <Route exact path="/" component={Products} />
-          <Route exact path="/products/new" component={NewProduct} />
-          <Route exact path="/products/edit/:id" component={EditProduct} />
-        </Switch>
-      </div>
+      <Provider store={store}>
+        <Header />
+        <div className="container mt-5">
+          <Switch>
+            <Route exact path="/" component={Products} />
+            <Route exact path="/products/new" component={NewProduct} />
+            <Route exact path="/products/edit/:id" component={EditProduct} />
+          </Switch>
+        </div>
+      </Provider>
     </Router>
   );
 }
