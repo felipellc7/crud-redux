8b481561e215320f937e1bdc7f31db75b50d53be
Felipe Jose Llerena Catalan <felipe@appmeup.co>
2020-06-22 23:57:39 -0500

    add product to state

--- src/actions/productActions.js
+++ src/actions/productActions.js
@@ -5,7 +5,27 @@ import {
 } from './../types'
 
 export function createNewProductAction(product) {
-  return () => {
-    console.log(product)
+  return dispatch => {
+    dispatch(addProduct())
+    try {
+      dispatch(addProductSuccess(product))
+    } catch (error) {
+      dispatch(addProductError(true))
+    }
   }
 }
+
+const addProduct = () => ({
+  type: ADD_PRODUCT,
+  payload: true
+})
+
+const addProductSuccess = product => ({
+  type: ADD_PRODUCT_SUCCESS,
+  payload: product
+})
+
+const addProductError = status => ({
+  type: ADD_PRODUCT_ERROR,
+  payload: status
+})
