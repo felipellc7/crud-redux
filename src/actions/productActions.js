3e38d009ba27fe318318ae4990e87c389cdc3bd9
Felipe Jose Llerena Catalan <felipe@appmeup.co>
2020-06-23 22:00:27 -0500

    save products in API

--- src/actions/productActions.js
+++ src/actions/productActions.js
@@ -3,13 +3,16 @@ import {
   ADD_PRODUCT_SUCCESS,
   ADD_PRODUCT_ERROR
 } from './../types'
+import axiosClient from './../config/axios'
 
 export function createNewProductAction(product) {
-  return dispatch => {
+  return async dispatch => {
     dispatch(addProduct())
     try {
+      await axiosClient.post('/products', product)
       dispatch(addProductSuccess(product))
     } catch (error) {
+      console.log(error)
       dispatch(addProductError(true))
     }
   }
